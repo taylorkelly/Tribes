479f31096cfd6bf39fc1645b72f52c968583d521
Taylor <taylor.kelly@me.com>
2013-04-30 00:22:57 -0400

    Show longevity not in seconds

--- core/src/main/java/com/first/tribes/core/being/Village.java
+++ core/src/main/java/com/first/tribes/core/being/Village.java
@@ -29,8 +29,8 @@ import pythagoras.f.Rectangle;
 public class Village implements Updatee {
 
 
-    public static final float MIN_INTELLIGENCE_TO_BUILD_IRRIGATION_PIPE = 2f;
-    public static final float INTELLIGENCE_BASELINE = 0.20f;
+    public static final float MIN_INTELLIGENCE_TO_BUILD_IRRIGATION_PIPE = 4f;
+    public static final float INTELLIGENCE_BASELINE = 0.22f;
     public static final float POSITION_DEVIATION = 10.0f;
     public static final float REPRODUCTIVE_BASE_RATE = 0.02f;
     public static final float REPRODUCTIVE_MODIFIER = 0.6f;
@@ -298,10 +298,10 @@ public class Village implements Updatee {
         Tile tile = world.tileAt(villager.xPos, villager.yPos);
         float foodGathered = Math.min(tile.numFood, requestedFood * (float) Math.pow(1 - villager.personality.intelligence(), 2));
 
-        if (villager.personality.intelligence() > FOOD_PRODUCTION_THRESHOLD) {
-            float foodProduced = (villager.personality.intelligence() - FOOD_PRODUCTION_THRESHOLD) * FOOD_PRODUCTION_RATE;
-            tile.numFood += foodProduced;
-        }
+//        if (villager.personality.intelligence() > FOOD_PRODUCTION_THRESHOLD) {
+//            float foodProduced = (villager.personality.intelligence() - FOOD_PRODUCTION_THRESHOLD) * FOOD_PRODUCTION_RATE;
+//            tile.numFood += foodProduced;
+//        }
 
         tile.numFood -= foodGathered;
         foodGathered = foodGathered / (float) Math.pow(1 - villager.personality.intelligence(), 2);
@@ -381,7 +381,7 @@ public class Village implements Updatee {
             StringBuilder stats = new StringBuilder();
             stats.append("Avg Rep. Appeal: " + repAppeal);
             stats.append('\n');
-            stats.append("Avg Longevity: " + (longevity * Villager.MAX_AGE / 1000.0) + " secs");
+            stats.append("Avg Longevity: " + longevity);
             stats.append('\n');
             stats.append("Avg Intelligence: " + intelligence);
             stats.append('\n');
